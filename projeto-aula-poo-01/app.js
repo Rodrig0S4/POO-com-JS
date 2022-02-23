2b950e9329269d75fcd5f9146c87ead77f7b1ced
Rodrig0S4 <sdearaujo.rodrigo@gmail.com>
2022-02-23 16:44:48 -0300

    [Feature - Task: Exporting and Importing Literal Objects]

--- projeto-aula-poo-01/app.js
+++ projeto-aula-poo-01/app.js
@@ -1,16 +1,19 @@
 import modificador from "./modificador.js";
+import ingredientes from "./ingredientes.js";
 
-console.log(modificador);
+console.log(ingredientes);
+console.log(typeof ingredientes);
+console.log(Array.isArray(ingredientes));
 
-var ingredientes = ['mel', 'água', 'sal', 'mostarda'];
-var resultadoCapitalizado = capitalizar(ingredientes);
-var resultadoOrdenacao = ordenar(resultadoCapitalizado);
-var resultadoCaixaAlta = modificador.caixaAlta(ingredientes);
+console.log(ingredientes[0].valor);
 
-console.log(resultadoCapitalizado);
-console.log(resultadoOrdenacao);
-console.log(resultadoCaixaAlta);
+// var ingredientes = ['mel', 'água', 'sal', 'mostarda'];
+// var resultadoCapitalizado = capitalizar(ingredientes);
+// var resultadoOrdenacao = ordenar(resultadoCapitalizado);
+// var resultadoCaixaAlta = modificador.caixaAlta(ingredientes);
 
-
+// console.log(resultadoCapitalizado);
+// console.log(resultadoOrdenacao);
+// console.log(resultadoCaixaAlta);
 
 //console.log(modificado.sort());
